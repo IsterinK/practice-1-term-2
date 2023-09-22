2dd5e93626522fe3a4cd2a231b1a2226ff8b4db8
Esteban Patiño <esteban.pg45@gmail.com>
2023-09-22 17:54:16 -0500

    # Issue 2, Show the information and the state of the checkbox and the switch

--- src/screens/SwitchCheckBoxComponent.js
+++ src/screens/SwitchCheckBoxComponent.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useState } from 'react';
-import { View, Text, TextInput, Switch } from 'react-native';
+import { View, Text, TextInput, Switch, Button } from 'react-native';
 import Checkbox from 'expo-checkbox';
 
 const SwitchCheckBoxComponent = () => {
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+
     const [estaActivo, setEstaActivo] = useState(false);
     const [isChecked, setIsChecked] = useState(false);
 
@@ -14,9 +17,33 @@ const SwitchCheckBoxComponent = () => {
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
     };
+
+    const handleSubmit = () => {
+        console.log('Nombre:', nombre);
+        console.log('Email:', email);
+        console.log('Activo:', estaActivo);
+        console.log('CheckBox:', isChecked);
+    };
+        
         
   return (
     <View>
+        <Text style={{marginBottom: 5}}>Nombre:</Text>
+        <TextInput
+            placeholder="Ingresa tu nombre"
+            style={{borderColor: "black", borderWidth: 1, borderRadius: 10, width: 200, textAlign: 'center',
+            marginBottom: 5}}
+            value={nombre}
+            onChangeText={(text) => setNombre(text)}
+        />
+        <Text style={{marginBottom: 5}}>Email:</Text>
+        <TextInput
+            placeholder="Ingresa tu email"
+            style={{borderColor: "black", borderWidth: 1, borderRadius: 10, width: 200, textAlign: 'center',
+            marginBottom: 5}}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+        />
         <View style={{ flexDirection: 'row', alignItems:
             'center' }}>
             <Switch
@@ -32,6 +59,12 @@ const SwitchCheckBoxComponent = () => {
             />
             <Text>CheckBox</Text>
         </View>
+
+        <Button
+            
+            title="Enviar"
+            onPress={handleSubmit}
+        />
     </View>
   )
 }
